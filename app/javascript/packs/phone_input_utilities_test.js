697b2bb3313bffdf1738184e9da9ca4cf71eab36
Tim Breitkreutz <tim.breitkreutz@drip.com>
2021-02-14 12:24:10 -0500

    split up the test

---
+++ app/javascript/packs/phone_input_utilities_test.js
@@ -0,0 +1,23 @@
+/*jslint browser:true*/
+/*global
+   chai
+   describe
+   expect
+   it
+ */
+
+import("/javascripts/phone_input_utilities.js").then(
+    function (pu) {
+        describe("camelCase", function () {
+            it("should change strings to camelcase", function () {
+                expect(pu.toCamelCase("ab-cd")).to.equal("abCd");
+                expect(pu.toCamelCase("abCd")).to.equal("abCd");
+                expect(pu.toCamelCase("")).to.equal("");
+                expect(pu.toCamelCase(undefined)).to.equal(undefined);
+                expect(pu.toCamelCase(1.5)).to.equal(undefined);
+                expect(pu.toCamelCase([1, 3])).to.equal(undefined);
+                expect(pu.toCamelCase({a: 5})).to.equal(undefined);
+            });
+        });
+    }
+);
